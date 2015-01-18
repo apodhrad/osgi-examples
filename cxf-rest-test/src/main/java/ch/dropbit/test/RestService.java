8ced16d8bc3903cf80d8acb0707a72e5e547313f
Andrej Podhradsky <apodhrad@redhat.com>
2015-01-18 22:34:17 +0100

    Added SecurityContext to rest service

--- cxf-rest-test/src/main/java/ch/dropbit/test/RestService.java
+++ cxf-rest-test/src/main/java/ch/dropbit/test/RestService.java
@@ -3,12 +3,14 @@ package ch.dropbit.test;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.SecurityContext;
 
 @Path("rest")
 public interface RestService {
 
     @GET
     @Path("hello/{name}")
-    public String handleGet(@PathParam("name") String name);
+    public String handleGet(@PathParam("name") String name, @Context SecurityContext context);
 
 }
--- cxf-rest-test/src/main/java/ch/dropbit/test/RestServiceImpl.java
+++ cxf-rest-test/src/main/java/ch/dropbit/test/RestServiceImpl.java
@@ -1,8 +1,13 @@
 package ch.dropbit.test;
 
+import javax.ws.rs.core.SecurityContext;
+
 public class RestServiceImpl implements RestService {
 
-	public String handleGet(String name) {
+	public String handleGet(String name, SecurityContext context) {
+		if(context.getUserPrincipal() != null) {
+			name += "-" + context.getUserPrincipal();
+		}
 		return String.format("Hi %s, Karaf and CXF is cool.", name);
 	}
 }
