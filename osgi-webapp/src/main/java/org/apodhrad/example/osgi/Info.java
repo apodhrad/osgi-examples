ade7f0283517b454c3211273a5dfa4c440ef61d2
Andrej Podhradsky <apodhrad@redhat.com>
2015-01-19 21:00:17 +0100

    Added security to osgi-webapp

--- osgi-webapp/src/main/java/org/apodhrad/example/osgi/Info.java
+++ osgi-webapp/src/main/java/org/apodhrad/example/osgi/Info.java
@@ -1,31 +1,26 @@
 package org.apodhrad.example.osgi;
 
-import java.awt.print.Book;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.security.Principal;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Invocation;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Response;
 
 /**
- * Servlet implementation class MyServlet
+ * Servlet implementation class Info
  */
-@WebServlet("/MyServlet")
-public class MyServlet extends HttpServlet {
+@WebServlet("/Info")
+public class Info extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public MyServlet() {
+    public Info() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -34,18 +29,15 @@ public class MyServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Client client = ClientBuilder.newBuilder().newClient();
-		
-		WebTarget target = client.target("http://localhost:8181/cxf/dropbit");
-		target = target.path("rest").path("hello").path("World");
-		 
-		Invocation.Builder builder = target.request();
-		Response r = builder.get();
-		String greet = builder.get(String.class);
+		Principal principal = request.getUserPrincipal();
+		String name = "anonym";
+		if(principal != null) {
+			name = principal.getName();
+		}
 		
 		PrintWriter out = response.getWriter();
-		out.println("Hello MyServlet!");
-		out.println("\nGreet: " + greet);
+		out.println("Hello Info! " + principal + " / " + principal.getClass());
+		out.println("\nGreet: " + name);
 		out.flush();
 		out.close();
 	}
--- osgi-webapp/src/main/java/org/apodhrad/example/osgi/logout.java
+++ osgi-webapp/src/main/java/org/apodhrad/example/osgi/logout.java
@@ -1,31 +1,23 @@
 package org.apodhrad.example.osgi;
 
-import java.awt.print.Book;
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Invocation;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Response;
 
 /**
- * Servlet implementation class MyServlet
+ * Servlet implementation class logout
  */
-@WebServlet("/MyServlet")
-public class MyServlet extends HttpServlet {
+@WebServlet("/logout")
+public class logout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public MyServlet() {
+    public logout() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -34,27 +26,17 @@ public class MyServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Client client = ClientBuilder.newBuilder().newClient();
-		
-		WebTarget target = client.target("http://localhost:8181/cxf/dropbit");
-		target = target.path("rest").path("hello").path("World");
-		 
-		Invocation.Builder builder = target.request();
-		Response r = builder.get();
-		String greet = builder.get(String.class);
-		
-		PrintWriter out = response.getWriter();
-		out.println("Hello MyServlet!");
-		out.println("\nGreet: " + greet);
-		out.flush();
-		out.close();
+		response.setHeader("Cache-Control", "no-cache, no-store");
+		response.setHeader("Pragma", "no-cache");
+		request.getSession().invalidate();
+		response.sendRedirect(request.getContextPath());
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 
 }
